b05ade51e57e037a21e1328f1e6893f43864422f
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-28 20:46:14 -0700

    complete lab

--- server/models.py
+++ server/models.py
@@ -11,6 +11,15 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
+employee_meetings = db.Table(
+    'employees_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey('employees.id')
+              , primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey('meetings.id')
+              , primary_key=True)
+)
+
 class Employee(db.Model):
     __tablename__ = 'employees'
 
@@ -18,6 +27,11 @@ class Employee(db.Model):
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    meetings = db.relationship('Meeting', secondary=employee_meetings, back_populates = 'employees')
+    assignments = db.relationship('Assignment', back_populates='employee', cascade='all, delete-orphan')
+    
+    projects = association_proxy('assignments', 'project',
+        creator=lambda project_obj: Assignment(project=project_obj))
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
@@ -30,9 +44,10 @@ class Meeting(db.Model):
     scheduled_time = db.Column(db.DateTime)
     location = db.Column(db.String)
 
+    employees = db.relationship('Employee', secondary=employee_meetings, back_populates = 'meetings')
+
     def __repr__(self):
         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
-
 
 class Project(db.Model):
     __tablename__ = 'projects'
@@ -41,5 +56,27 @@ class Project(db.Model):
     title = db.Column(db.String)
     budget = db.Column(db.Integer)
 
+    assignments = db.relationship('Assignment', back_populates = 'project', cascade = 'all, delete-orphan')
+    
+    employees = association_proxy('assignments', 'employee',
+        creator=lambda employee_obj: Assignment(employee=employee_obj))
     def __repr__(self):
         return f'<Review {self.id}, {self.title}, {self.budget}>'
+    
+    
+class Assignment(db.Model):
+    __tablename__ = 'assignments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    role = db.Column(db.String)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+
+    employee = db.relationship('Employee', back_populates = 'assignments')
+    project = db.relationship('Project', back_populates = 'assignments')
+
+    def __repr__(self):
+        return f'<Assignment {self.id}, {self.role}, {self.start_date}, {self.end_date}, {self.employee.name}, {self.project.title}>'
